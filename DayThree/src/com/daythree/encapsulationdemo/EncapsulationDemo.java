707fc4ce55241cce1d2344cd49100f60e633a9cc
Varsha Kumari <varshakumari0598@gmail.com>
2023-09-13 15:22:00 +0530

    Day3

---
+++ DayThree/src/com/daythree/encapsulationdemo/EncapsulationDemo.java
@@ -0,0 +1,21 @@
+package com.daythree.encapsulationdemo;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+
+		OopsDemo oops=new OopsDemo();
+				
+		oops.setAge(23);
+		oops.setName("Tushar");
+		oops.setSerialNum(2);
+		
+		System.out.println(oops.getAge());
+		System.out.println(oops.getSerialNum());
+		System.out.println(oops.getName());
+		
+		System.out.println(oops);
+
+	}
+
+}
