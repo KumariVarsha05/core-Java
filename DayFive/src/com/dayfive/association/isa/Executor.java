56fa9426dc06bc76ddd3b60196f970b1363fa2e1
Varsha Kumari <varshakumari0598@gmail.com>
2023-09-13 16:22:51 +0530

    Day5

---
+++ DayFive/src/com/dayfive/association/isa/Executor.java
@@ -0,0 +1,17 @@
+// Usage of the "IS-A" relationship 
+package com.dayfive.association.isa;
+
+public class Executor {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee("Sagar Pawar", 101, "IT");
+		Manager manager = new Manager("Ankit Kale", 201, "Hr", 8);
+
+		System.out.println(employee);
+		System.out.println("---------------------------------------------------------------");
+		System.out.println(manager);
+
+	}
+
+}
---
+++ DayFive/src/com/dayfive/singleinheritence/SingleInheritenceDemo.java
@@ -0,0 +1,12 @@
+package com.dayfive.singleinheritence;
+
+public class SingleInheritenceDemo {
+
+	public static void main(String[] args) {
+
+		Student s1=new Student("Arnab",21,"Mumbai",20,"Mumbai",100,"ABC","CSE");
+		System.out.println(s1);
+
+	}
+
+}
