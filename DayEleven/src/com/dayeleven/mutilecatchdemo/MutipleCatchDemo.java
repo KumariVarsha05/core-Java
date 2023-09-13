10bb25903dc4ecf2e8ee98dc804906640e8570c5
Varsha Kumari <varshakumari0598@gmail.com>
2023-09-13 19:58:09 +0530

    Day11

---
+++ DayEleven/src/com/dayeleven/mutilecatchdemo/MutipleCatchDemo.java
@@ -0,0 +1,12 @@
+package com.dayeleven.mutilecatchdemo;
+
+public class MutipleCatchDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("I am in Main method");
+		Division.divide();
+
+	}
+
+}
---
+++ DayEleven/src/com/dayeleven/nestedcatchdemo/NestedDemo.java
@@ -0,0 +1,11 @@
+package com.dayeleven.nestedcatchdemo;
+
+public class NestedDemo {
+
+	public static void main(String[] args) {
+
+		NestedTryCatch.check();
+
+	}
+
+}
